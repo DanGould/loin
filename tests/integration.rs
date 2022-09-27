24179ae2c4e1256394cfe5e50abe9d609799916e
DanGould <d@ngould.dev>
2022-09-27 16:13:14 -0400

    Init one lnd

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,11 +1,15 @@
 #[cfg(all(feature = "test_paths"))]
 mod integration {
-    use std::process::Command;
+    use std::{
+        io::Write,
+        process::{Command, Stdio},
+    };
 
     use bitcoincore_rpc::{Auth, Client, RpcApi};
+    use tempfile::tempdir;
 
-    #[test]
-    fn test() {
+    #[tokio::test]
+    async fn test() {
         let compose_dir = format!("{}/tests/compose", env!("CARGO_MANIFEST_DIR"));
         println!("Running docker-compose at {}", compose_dir);
         Command::new("docker-compose")
@@ -15,8 +19,8 @@ mod integration {
             .arg("-d")
             .output()
             .expect("failed to docker-compose ... up");
-
-        // sanity check
+        std::thread::sleep(std::time::Duration::from_secs(3));
+        // bitcoin rpc sanity check
         let rpc = Client::new(
             "http://localhost:43782",
             Auth::UserPass("ceiwHEbqWI83".to_string(), "DwubwWsoo3".to_string()),
@@ -24,7 +28,32 @@ mod integration {
         .unwrap();
         assert!(rpc.get_best_block_hash().is_ok());
 
-        
+        let tmp_dir = tempdir().expect("Couldn't open tmp_dir");
+        let tmp_path = tmp_dir.path().to_str().expect("Invalid tmp_dir path");
+
+        Command::new("docker")
+            .arg("cp")
+            .arg("compose-merchant_lnd-1:/root/.lnd/tls.cert")
+            .arg(format!("{}/tls.cert", &tmp_path))
+            .output()
+            .expect("failed to copy tls.cert");
+
+        Command::new("docker")
+            .arg("cp")
+            .arg("compose-merchant_lnd-1:/data/chain/bitcoin/regtest/admin.macaroon")
+            .arg(format!("{}/admin.macaroon", &tmp_path))
+            .output()
+            .expect("failed to copy admin.macaroon");
+
+        // merchant lnd rpc sanity check
+        let address = "https://localhost:53281".to_string();
+        let cert_file = format!("{}/tls.cert", &tmp_path).to_string();
+        let macaroon_file = format!("{}/admin.macaroon", &tmp_path).to_string();
+
+        // Connecting to LND requires only address, cert file, and macaroon file
+        let mut client =
+            tonic_lnd::connect(address, cert_file, macaroon_file).await.expect("failed to connect");
+
         Command::new("docker-compose")
             .arg("--project-directory")
             .arg(compose_dir)
@@ -32,5 +61,7 @@ mod integration {
             .output()
             .expect("failed to docker-compose ... down");
         assert!(true);
+
+        tmp_dir.close().expect("Couldn't close tmp_dir");
     }
 }
